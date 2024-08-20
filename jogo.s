bf88e527c740857553abbb45cd2edbe2b70e0333
Marcelo Vitor <marcelovitor890@gmail.com>
2024-08-20 16:40:57 -0300

    Atualização

--- jogo.s
+++ jogo.s
@@ -1,10 +1,5 @@
-.data
-CHAR_POS: 	.half 0, 0
-OLD_CHAR_POS:	.half 0, 0
-
 .text
-SETUP:
-		la,a0,map
+SETUP:		la a0, map
 		li a1, 0
 		li a2, 0
 		li a3, 0
@@ -12,62 +7,66 @@ SETUP:
 		li a3, 1
 		call PRINT
 	
-GAME_LOOP:
-		call KEY2
-		
-		xori s0, s0, 1
+GAME_LOOP:	xori s0, s0, 1
 
-		la t0, CHAR_POS
 		la a0, char
-		lh a1, 0(t0)
-		lh a2, 2(t0)
+		li a1, 16
+		li a2, 0
 		mv a3, s0
 		call PRINT
-		li t0, 0XFF200604
+		
+		li t0, 0xFF200604
 		sw s0, 0(t0)
-
-		j GAME_LOOP
 	
-KEY2: 
-		li t1, 0xFF200000
-		lw t0, 0(t1)
-		andi t0, t0, 0x0001
-		beq t0, zero, FIM
-		lw t2, 4(t1)
-
-		li a0, 'a'
-		beq t2, t0, CHAR_ESQ
-
-		li t0, 'd'
-		beq t2, t0, CHAR_DIR
-
-FIM:
-		ret
-
-CHAR_ESQ:
-		la t0, CHAR_POS
-		la_t1, OLD_CHAR_POS
-		lw t2, 0(t0)
-		sw t2, 0(t1)
+#
+#	a0 = endereço imagem
+#	a1 = x
+#	a2 = y
+#	a3 = frame (0 ou 1)
+##
+#
+#	t0 = endereço do bitmap display
+#	t1 = endereço da imagem
+#	t2 = contador de linha
+#	t3 = contador de coluna
+#	t4 = largura
+#	t5 = altura
+#
+PRINT:		li t0, 0xFF0
+		add t0, t0, a3
+		slli t0, t0, 20
 		
-
-		lh t1, 0(t0)
-		addi t1, t1, -16
-		sh t1, 0(t0)
-		ret
+		add t0, t0, a1
 		
-CHAR_DIR:
-		la t0, CHAR_POS
-		la_t1, OLD_CHAR_POS
-		lw t2, 0(t0)
-		sw t2, 0(t1)
-
-		lh t1, 0(t0)
-		addi t1, t1, 16
-		sh t1, 0(t0)
-		ret
+		li t1, 320
+		mul t1, t1, a2
+		add t0, t0, a1
+		
+		addi t1, a0, 8
+		
+		mv t2, zero
+		mv t3, zero
+		
+		lw t4, 0(a0)
+		lw t5, 4(a0)
+	
+PRINT_LINHA:	lw t6, 0(t1)
+		sw t6, 0(t0)
+		
+		addi t0, t0, 4
+		addi t1, t1, 4
+		
+		addi t3, t3, 4
+		blt t3, t4, PRINT_LINHA
+		
+		addi t0, t0, 320
+		sub t0, t0, t4
+		
+		mv t3, zero
+		addi t2, t2, 1
+		bgt t5, t2, PRINT_LINHA
 		
 .data
-.include "sprites/tile.s"
+.include "sprites/flamengo.s"
 .include "sprites/map.s"
 .include "sprites/char.s"
